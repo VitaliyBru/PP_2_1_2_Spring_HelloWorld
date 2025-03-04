1613aa4b67c24a247296d0ebdd897296a7976a42
Vitaly Bryukhovetsky <bruhovet@list.ru>
2025-03-05 00:38:56 +0300

    Добавляет решение.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,19 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
+        AnnotationConfigApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 = applicationContext.getBean("helloworld", HelloWorld.class);
+        System.out.println("helloworld bean equals to helloworld bean2 is: " + bean.equals(bean2));
+
+        Cat beanCat1 = applicationContext.getBean("cat", Cat.class);
+        Cat beanCat2 = applicationContext.getBean("cat", Cat.class);
+        System.out.println("Beans cat beanCat1 equals to beanCat2 is: " + beanCat1.equals(beanCat2));
+
+        applicationContext.close();
     }
 }
